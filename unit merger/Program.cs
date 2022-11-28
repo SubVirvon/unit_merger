50fab6ed7aeaaac2a4fd3b4e8ee57d6a4d613639
Virvon <dan130205@gmail.com>
2022-11-28 11:13:10 +0300

    unit_merger

---
+++ unit merger/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Linq;
+
+namespace unit_merger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Warrior[] platoon1 = new Warrior[] 
+            {
+                new Warrior("Глеб"),
+                new Warrior("Борис"),
+                new Warrior("Михаил"),
+                new Warrior("Богдан"),
+                new Warrior("Константин"),
+                new Warrior("Евгений"),
+            };
+            Warrior[] platoon2 = new Warrior[]
+            {
+                new Warrior("Елисей"),
+                new Warrior("Алексей"),
+                new Warrior("Максим"),
+                new Warrior("Александр"),
+            };
+            Controller controller = new Controller(platoon1, platoon2);
+
+            controller.MergePlatoons("Б");
+            controller.ShowPlatoon2();
+
+            Console.ReadKey();
+        }
+    }
+
+    class Controller
+    {
+        private Warrior[] _platoon1;
+        private Warrior[] _platoon2;
+
+        public Controller(Warrior[] platoon1, Warrior[] platoon2)
+        {
+            _platoon1 = platoon1;
+            _platoon2 = platoon2;
+        }
+
+        public void MergePlatoons(string FirstSymbol)
+        {
+            _platoon2 = _platoon2.Union(_platoon1.Where(warrior => warrior.Name.StartsWith(FirstSymbol))).ToArray();
+        }
+
+        public void ShowPlatoon2()
+        {
+            foreach(var warrior in _platoon2)
+            {
+                Console.WriteLine(warrior.Name);
+            }
+        }
+    }
+
+    class Warrior
+    {
+        public string Name { get; private set; }
+
+        public Warrior(string name)
+        {
+            Name = name;
+        }
+    }
+}
